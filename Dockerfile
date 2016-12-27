d3d2507dac3ee06aed23e15d2249139cbdf3eff2
JUDITH Florian <florian.judith@economat-armees.fr>
2016-12-27 09:47:59 +0100

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,33 @@
+FROM debian:stable
+
+MAINTAINER Florian JUDITH <florian.judith.b@gmail.com>
+
+ENV TERM=xterm
+
+ENV DEBIAN_FRONTEND noninteractive
+
+RUN set -x && \
+    apt-get -y update && \
+    apt-get -y --no-install-recommends install \
+        crudini \
+        krb5-user \
+        libpam-krb5 \
+        winbind \
+        libpam-mount \
+        cifs-utils
+
+RUN apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+
+RUN env --unset=DEBIAN_FRONTEND
+
+COPY docker-entrypoint.sh /docker-entrypoint.sh
+
+EXPOSE 137 138 139 445
+
+ENTRYPOINT ["/docker-entrypoint.sh"]
+CMD ["bash"]
+
+
+
+
+
---
+++ docker-entrypoint.sh
@@ -0,0 +1,144 @@
+#! /bin/bash
+# Reference:
+# * https://wiki.debian.org/AuthenticatingLinuxWithActiveDirectory
+# * https://wiki.samba.org/index.php/Troubleshooting_Samba_Domain_Members
+
+set -e
+
+# Update loopback entry
+AD_USERNAME=${AD_USERNAME:-administrator}
+AD_PASSWORD=${AD_PASSWORD:-password}
+HOSTNAME=${HOSTNAME:-$(hostname)}
+IP_ADDRESS=${IP_ADDRESS:-}
+DOMAIN_NAME=${DOMAIN_NAME:-domain.loc}
+ADMIN_SERVER=${ADMIN_SERVER:-${DOMAIN_NAME,,}}
+KDC_SERVER=${KDC_SERVER:-${ADMIN_SERVER,,}}
+PASSWORD_SERVER=${PASSWORD_SERVER:-${ADMIN_SERVER,,}}
+
+ENCRYPTION_TYPES=${ENCRYPTION_TYPES:-rc4-hmac des3-hmac-sha1 des-cbc-crc}
+SECURITY=${SECURITY:-ads}
+REALM=${REALM:-${DOMAIN_NAME^^}}
+PASSWORD_SERVER=${PASSWORD_SERVER:-${DOMAIN_NAME,,}}
+WORKGROUP=${WORKGROUP:-${DOMAIN_NAME^^}}
+WINBIND_SEPARATOR=${WINBIND_SEPARATOR:-"\\"}
+IDMAP_UID=${IDMAP_UID:-10000-20000}
+IDMAP_GID=${IDMAP_GID:-10000-20000}
+WINBIND_ENUM_USERS=${WINBIND_ENUM_USERS:-yes}
+WINBIND_ENUM_GROUPS=${WINBIND_ENUM_GROUPS:-yes}
+TEMPLATE_HOMEDIR=${TEMPLATE_HOMEDIR:-/home/%D/%U}
+TEMPLATE_SHELL=${TEMPLATE_SHELL:-/bin/bash}
+CLIENT_USE_SPNEGO=${CLIENT_USE_SPNEGO:-yes}
+CLIENT_NTLMV2_AUTH=${CLIENT_NTLMV2_AUTH:-yes}
+ENCRYPT_PASSWORDS=${ENCRYPT_PASSWORDS:-yes}
+WINDBIND_USE_DEFAULT_DOMAIN=${WINBIND_USE_DEFAULT_DOMAIN:-yes}
+RESTRICT_ANONYMOUS=${RESTRICT_ANONYMOUS:-2}
+DOMAIN_MASTER=${DOMAIN_MASTER:-no}
+LOCAL_MASTER=${LOCAL_MASTER:-no}
+PREFERRED_MASTER=${PREFERRED_MASTER:-no}
+OS_LEVEL=${OS_LEVEL:-0}
+WINS_SUPPORT=${WINS_SUPPORT:-no}
+WINS_SERVER=${WINS_SERVER:-127.0.0.1}
+DNS_PROXY=${DNS_PROXY:-no}
+LOG_FILE=${LOG_FILE:-/var/log/samba/log.%m}
+MAX_LOG_SIZE=${MAX_LOG_SIZE:-1000}
+SYSLOG_ONLY=${SYSLOG_ONLY:-no}
+SYSLOG=${SYSLOG:-0}
+PANIC_ACTION=${PANIC_ACTION:-/usr/share/samba/panic-action %d}
+
+
+SAMBA_CONF=/etc/samba/smb.conf
+
+# Below not working as of Docker 1.1.0
+# Ref: https://forums.docker.com/t/container-losing-volume-etc-hosts-when-file-is-edited/10038/3
+# sed -i "s#^\(127\.0\.0\.1\s*locahost\).*\$#\1 $HOSTNAME $HOSTNAME.$DOMAIN_NAME#" /etc/hosts
+#
+# Instead Use docker run --add-host $(hostname).domain.loc:127.0.0.1 --add-host $(hostname):127.0.0.1 alpine
+
+
+# Initialize Kerberos authentication
+if [[ ! -f /etc/krb5.conf.original ]]; then
+	mv /etc/krb5.conf /etc/krb5.conf.original
+fi
+echo "
+[logging]
+    Default = FILE:/var/log/krb5.log
+
+[libdefaults]
+	default_realm = ${DOMAIN_NAME^^}
+    ticket_lifetime = 24000
+    clock-skew = 300
+    default_tkt_enctypes = ${ENCRYPTION_TYPES}
+    default_tgs_enctypes = ${ENCRYPTION_TYPES}
+#   dns_lookup_realm = false
+#   dns_lookup_kdc = true
+
+[realms]
+    ${DOMAIN_NAME^^} = {
+        kdc = ${KDC_SERVER,,}:88
+        admin_server = ${ADMIN_SERVER,,}:464
+        default_domain = ${DOMAIN_NAME,,}       
+}
+
+[realms]
+    ${DOMAIN_NAME,,} = {
+        kdc = ${KDC_SERVER,,}:88
+        admin_server = ${ADMIN_SERVER,,}:464
+        default_domain = ${DOMAIN_NAME,,}       
+}
+
+[domain_realm]
+    .${DOMAIN_NAME,,} = ${DOMAIN_NAME^^}
+    ${DOMAIN_NAME,,} = ${DOMAIN_NAME^^}
+" > /etc/krb5.conf
+
+# Rename original smb.conf
+if [[ ! -f /etc/samba/smb.conf.original ]]; then
+	mv /etc/samba/smb.conf /etc/samba/smb.conf.original
+	touch $SAMBA_CONF
+fi
+
+crudini --set $SAMBA_CONF global "security" "$SECURITY"
+crudini --set $SAMBA_CONF global "realm" "$REALM"
+crudini --set $SAMBA_CONF global "password server" "$PASSWORD_SERVER"
+crudini --set $SAMBA_CONF global "workgroup" "$WORKGROUP"
+#crudini --set $SAMBA_CONF global "winbind separator" "$WINBIND_SEPARATOR"
+crudini --set $SAMBA_CONF global "idmap uid" "$IDMAP_UID"
+crudini --set $SAMBA_CONF global "idmap gid" "$IDMAP_GID"
+crudini --set $SAMBA_CONF global "winbind enum users" "$WINBIND_ENUM_USERS"
+crudini --set $SAMBA_CONF global "winbind enum groups" "$WINBIND_ENUM_GROUPS"
+crudini --set $SAMBA_CONF global "template homedir" "$TEMPLATE_HOMEDIR"
+crudini --set $SAMBA_CONF global "template shell" "$TEMPLATE_SHELL"
+crudini --set $SAMBA_CONF global "client use spnego" "$CLIENT_USE_SPNEGO"
+crudini --set $SAMBA_CONF global "client ntlmv2 auth" "$CLIENT_NTLMV2_AUTH"
+crudini --set $SAMBA_CONF global "encrypt passwords" "$ENCRYPT_PASSWORDS"
+crudini --set $SAMBA_CONF global "winbind use default domain" "$WINDBIND_USE_DEFAULT_DOMAIN"
+crudini --set $SAMBA_CONF global "restrict anonymous" "$RESTRICT_ANONYMOUS"
+crudini --set $SAMBA_CONF global "domain master" "$DOMAIN_MASTER"
+crudini --set $SAMBA_CONF global "local master" "$LOCAL_MASTER"
+crudini --set $SAMBA_CONF global "preferred master" "$PREFERRED_MASTER"
+crudini --set $SAMBA_CONF global "os level" "$OS_LEVEL"
+crudini --set $SAMBA_CONF global "wins support" "$WINS_SUPPORT"
+crudini --set $SAMBA_CONF global "wins server" "$WINS_SERVER"
+crudini --set $SAMBA_CONF global "dns proxy" "$DNS_PROXY"
+crudini --set $SAMBA_CONF global "log file" "$LOG_FILE"
+crudini --set $SAMBA_CONF global "max log size" "$MAX_LOG_SIZE"
+crudini --set $SAMBA_CONF global "syslog only" "$SYSLOG_ONLY"
+crudini --set $SAMBA_CONF global "syslog" "$SYSLOG"
+crudini --set $SAMBA_CONF global "panic action" "$PANIC_ACTION"
+
+/etc/init.d/winbind stop
+/etc/init.d/samba restart
+/etc/init.d/winbind start
+
+sleep 5
+
+echo --------------------------------------------------
+echo 'Generating Kerberos ticket'
+echo --------------------------------------------------
+echo $AD_PASSWORD | kinit $AD_USERNAME@$REALM
+klist
+
+echo --------------------------------------------------
+echo 'Regestering to Active Directory'
+echo --------------------------------------------------
+net ads join -U $AD_USERNAME%$AD_PASSWORD
