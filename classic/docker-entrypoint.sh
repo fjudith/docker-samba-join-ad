61a57c7fdd9487145b9b95d091c0176042dd61b9
JUDITH Florian <florian.judith@economat-armees.fr>
2017-07-07 21:39:18 +0200

    added workgroup based realm

--- classic/docker-entrypoint.sh
+++ classic/docker-entrypoint.sh
@@ -101,7 +101,12 @@ cat > /etc/krb5.conf << EOL
         admin_server = $(echo ${ADMIN_SERVER,,} | awk '{print $1}')
         default_domain = ${DOMAIN_NAME,,}
     }
-
+    ${WORKGROUP^^} = {
+        kdc = $(echo ${ADMIN_SERVER,,} | awk '{print $1}')
+        admin_server = $(echo ${ADMIN_SERVER,,} | awk '{print $1}')
+        default_domain = ${DOMAIN_NAME^^}       
+    }
+    
 [domain_realm]
     .${DOMAIN_NAME,,} = ${DOMAIN_NAME^^}
     ${DOMAIN_NAME,,} = ${DOMAIN_NAME^^}
@@ -114,16 +119,57 @@ if [[ ! -f /etc/samba/smb.conf.original ]]; then
 	touch $SAMBA_CONF
 fi
 
-crudini --set $SAMBA_CONF global "server string" "$SERVER_STRING"
+echo --------------------------------------------------
+echo "Generating Samba configuration: \"$SAMBA_CONF\""
+echo --------------------------------------------------
 crudini --set $SAMBA_CONF global "vfs objects" "acl_xattr"
 crudini --set $SAMBA_CONF global "map acl inherit" "yes"
 crudini --set $SAMBA_CONF global "store dos attributes" "yes"
+crudini --set $SAMBA_CONF global "guest account" "$GUEST_USERNAME"
 
-crudini --set $SAMBA_CONF global "security" "$SECURITY"
-crudini --set $SAMBA_CONF global "realm" "$REALM"
-#crudini --set $SAMBA_CONF global "password server" "$PASSWORD_SERVER"
 crudini --set $SAMBA_CONF global "workgroup" "$WORKGROUP"
-#crudini --set $SAMBA_CONF global "winbind separator" "$WINBIND_SEPARATOR"
+crudini --set $SAMBA_CONF global "server string" "$SERVER_STRING"
+
+# Add the IPs / subnets allowed acces to the server in general.
+crudini --set $SAMBA_CONF global "hosts allow" "$HOSTS_ALLOW"
+
+# log files split per-machine.
+crudini --set $SAMBA_CONF global "log file" "$LOG_FILE"
+
+# Enable debug
+crudini --set $SAMBA_CONF global "log level" "$LOG_LEVEL"
+
+# Maximum size per log file, then rotate.
+crudini --set $SAMBA_CONF global "max log size" "$MAX_LOG_SIZE"
+
+# Active Directory
+crudini --set $SAMBA_CONF global "security" "$SECURITY"
+crudini --set $SAMBA_CONF global "encrypt passwords" "$ENCRYPT_PASSWORDS"
+crudini --set $SAMBA_CONF global "passdb backend" "tdbsam"
+crudini --set $SAMBA_CONF global "realm" "$REALM"
+
+# Disable Printers.
+crudini --set $SAMBA_CONF global "printcap name" "/dev/null"
+crudini --set $SAMBA_CONF global "panic action" "no"
+crudini --set $SAMBA_CONF global "cups options" "raw"
+
+# Name resolution order
+crudini --set $SAMBA_CONF global "name resolve order" "$NAME_RESOLVE_ORDER"
+
+# Performance Tuning
+crudini --set $SAMBA_CONF global "socket options" "$SOCKET_OPTIONS"
+crudini --set $SAMBA_CONF global "read raw" "$READ_RAW"
+crudini --set $SAMBA_CONF global "write raw" "$WRITE_RAW"
+crudini --set $SAMBA_CONF global "oplocks" "$OPLOCKS"
+crudini --set $SAMBA_CONF global "level2 oplocks" "$LEVEL2_OPLOCKS"
+crudini --set $SAMBA_CONF global "kernel oplocks" "$KERNEL_OPLOCKS"
+crudini --set $SAMBA_CONF global "max xmit" "$MAX_XMIT"
+crudini --set $SAMBA_CONF global "dead time" "$DEAD_TIME"
+
+# Point to specific kerberos server
+crudini --set $SAMBA_CONF global "password server" "$PASSWORD_SERVER"
+
+# #crudini --set $SAMBA_CONF global "winbind separator" "$WINBIND_SEPARATOR"
 crudini --set $SAMBA_CONF global "winbind uid" "$WINBIND_UID"
 crudini --set $SAMBA_CONF global "winbind gid" "$WINBIND_GID"
 crudini --set $SAMBA_CONF global "winbind use default domain" "$WINDBIND_USE_DEFAULT_DOMAIN"
@@ -135,13 +181,14 @@ crudini --set $SAMBA_CONF global "client use spnego" "$CLIENT_USE_SPNEGO"
 crudini --set $SAMBA_CONF global "client ntlmv2 auth" "$CLIENT_NTLMV2_AUTH"
 crudini --set $SAMBA_CONF global "encrypt passwords" "$ENCRYPT_PASSWORDS"
 crudini --set $SAMBA_CONF global "server signing" "$SERVER_SIGNING"
+crudini --set $SAMBA_CONF global "smb encrypt" "$SMB_ENCRYPT"
 crudini --set $SAMBA_CONF global "restrict anonymous" "$RESTRICT_ANONYMOUS"
 crudini --set $SAMBA_CONF global "domain master" "$DOMAIN_MASTER"
 crudini --set $SAMBA_CONF global "local master" "$LOCAL_MASTER"
 crudini --set $SAMBA_CONF global "preferred master" "$PREFERRED_MASTER"
 crudini --set $SAMBA_CONF global "os level" "$OS_LEVEL"
-crudini --set $SAMBA_CONF global "wins support" "$WINS_SUPPORT"
-crudini --set $SAMBA_CONF global "wins server" "$WINS_SERVER"
+# crudini --set $SAMBA_CONF global "wins support" "$WINS_SUPPORT"
+# crudini --set $SAMBA_CONF global "wins server" "$WINS_SERVER"
 crudini --set $SAMBA_CONF global "dns proxy" "$DNS_PROXY"
 crudini --set $SAMBA_CONF global "log level" "$LOG_LEVEL"
 crudini --set $SAMBA_CONF global "debug timestamp" "$DEBUG_TIMESTAMP"
@@ -149,39 +196,17 @@ crudini --set $SAMBA_CONF global "log file" "$LOG_FILE"
 crudini --set $SAMBA_CONF global "max log size" "$MAX_LOG_SIZE"
 crudini --set $SAMBA_CONF global "syslog only" "$SYSLOG_ONLY"
 crudini --set $SAMBA_CONF global "syslog" "$SYSLOG"
-crudini --set $SAMBA_CONF global "panic action" "$PANIC_ACTION"
-crudini --set $SAMBA_CONF global "hosts allow" "$HOSTS_ALLOW"
-crudini --set $SAMBA_CONF global "socket options" "$SOCKET_OPTIONS"
-crudini --set $SAMBA_CONF global "read raw" "$READ_RAW"
-crudini --set $SAMBA_CONF global "write raw" "$WRITE_RAW"
-crudini --set $SAMBA_CONF global "oplocks" "$OPLOCKS"
-crudini --set $SAMBA_CONF global "level2 oplocks" "$LEVEL2_OPLOCKS"
-crudini --set $SAMBA_CONF global "kernel oplocks" "$KERNEL_OPLOCKS"
-crudini --set $SAMBA_CONF global "max xmit" "$MAX_XMIT"
-crudini --set $SAMBA_CONF global "dead time" "$DEAD_TIME"
-
-# Disable printing error log messages when CUPS is not installed.
-crudini --set $SAMBA_CONF global "printcap name" "/etc/printcap"
-crudini --set $SAMBA_CONF global "panic action" "no"
+# crudini --set $SAMBA_CONF global "panic action" "$PANIC_ACTION"
+# crudini --set $SAMBA_CONF global "hosts allow" "$HOSTS_ALLOW"
 
 # Inherit groups in groups
-crudini --set $SAMBA_CONF global "winbind nested groups" "yes"
+crudini --set $SAMBA_CONF global "winbind nested groups" "no"
 crudini --set $SAMBA_CONF global "winbind refresh tickets" "yes"
 crudini --set $SAMBA_CONF global "winbind offline logon" "true"
 
-# No shell access
-crudini --set $SAMBA_CONF global "template shell" "/bin/false"
-crudini --set $SAMBA_CONF global "client use spnego" "yes"
-crudini --set $SAMBA_CONF global "client ntlmv2 auth" "yes"
-crudini --set $SAMBA_CONF global "encrypt passwords" "yes"
-crudini --set $SAMBA_CONF global "restrict anonymous" "2"
-
-# Name resolution order
-crudini --set $SAMBA_CONF global "name resolve order" "$NAME_RESOLVE_ORDER"
-
 # home shared directory (restricted to owner)
 crudini --set $SAMBA_CONF home "comment" "Home Directories"
-crudini --set $SAMBA_CONF home "path" "/home"
+crudini --set $SAMBA_CONF home "path" "/home/"
 crudini --set $SAMBA_CONF home "public" "yes"
 crudini --set $SAMBA_CONF home "guest ok" "no"
 crudini --set $SAMBA_CONF home "read only" "no"
@@ -191,11 +216,12 @@ crudini --set $SAMBA_CONF home "directory mask" "0777"
 crudini --set $SAMBA_CONF home "browseable" "yes"
 crudini --set $SAMBA_CONF home "printable" "no"
 crudini --set $SAMBA_CONF home "oplocks" "yes"
+#crudini --set $SAMBA_CONF home "valid users" "%S"
 
 # public shared directory (unrestricted)
 mkdir -p "/usr/share/public"
 crudini --set $SAMBA_CONF public "comment" "Public Directories"
-crudini --set $SAMBA_CONF public "path" "/usr/share/public"
+crudini --set $SAMBA_CONF public "path" "/usr/share/public/"
 crudini --set $SAMBA_CONF public "public" "yes"
 crudini --set $SAMBA_CONF public "guest ok" "no"
 crudini --set $SAMBA_CONF public "read only" "no"
@@ -209,7 +235,7 @@ crudini --set $SAMBA_CONF public "oplocks" "yes"
 # private shared directory (restricted)
 mkdir -p "/usr/share/private"
 crudini --set $SAMBA_CONF private "comment" "Private Directories"
-crudini --set $SAMBA_CONF private "path" "/usr/share/private"
+crudini --set $SAMBA_CONF private "path" "/usr/share/private/"
 crudini --set $SAMBA_CONF private "public" "yes"
 crudini --set $SAMBA_CONF private "guest ok" "no"
 crudini --set $SAMBA_CONF private "read only" "no"
